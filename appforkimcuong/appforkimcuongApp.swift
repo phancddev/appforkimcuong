8aa9dd820178b918ba1cf4a0841c4b957c73f936
Công Dũng Phan <phancd@phancds-MacBook-Pro.local>
2025-02-20 00:01:21 +0700

    up 1

---
+++ appforkimcuong/appforkimcuongApp.swift
@@ -0,0 +1,17 @@
+//
+//  appforkimcuongApp.swift
+//  appforkimcuong
+//
+//  Created by Công Dũng Phan on 19/2/25.
+//
+
+import SwiftUI
+
+@main
+struct appforkimcuongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
